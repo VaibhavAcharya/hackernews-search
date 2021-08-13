14beeff78405ec3a6affb305e0c2bc59416a9474
Vaibhav <vaibhavacharya111@gmail.com>
2021-08-13 11:19:51 +0530

    run: prettify

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 ## [Hackernews Search](http://hackernews-search.vercel.app)
+
 Web Application to search HackerNews posts easily.
 Using [HN Search API](https://hn.algolia.com/api).
 
 ## Tech Stack
+
 Next.js (React) & vanilla CSS.
 
 Extra libraries utilized: date-fns & Prettier.
+
 ## To start on local machine
 
 - Clone this repository.
 
 - Install dependencies :
+
 ```bash
 yarn
 ```
 
 - Start development server :
+
 ```bash
 yarn dev
 ```
--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,11 @@ class _Document extends Document {
       <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap"
             rel="stylesheet"
